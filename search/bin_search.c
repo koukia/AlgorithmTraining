6be5cd95ce1850c41065b98b76d605f5dcc8b1c2
kohki mori <tk011875@gmail.com>
2018-07-20 11:09:04 +0900

    fix:bin_search

--- search/bin_search.c
+++ search/bin_search.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define INPUT_SIZE 16
+#define INPUT_SIZE 10
 int input[INPUT_SIZE];
 
 int binary_search(int key)
@@ -11,30 +11,52 @@ int binary_search(int key)
   int search_right = INPUT_SIZE - 1;
   int pivot;
 
-  while (search_left < search_right) {
+  while (search_left <= search_right) {
     pivot = (search_right + search_left) / 2;
+    printf("pivot is %d\n", pivot);
+
     if (key == input[pivot]) {
       return pivot;
-    }
-    if (pivot == search_right || pivot == search_left)
-      break;
-    if (key < input[pivot])
-      search_right = pivot;
+    }else if (key < input[pivot])
+      search_right = pivot - 1;
     else
-      search_left = pivot;
+      search_left = pivot + 1;
   }
+  return -1;
 }
 
+void test_bin_search(int target, int expect)
+{
+  int result = binary_search(target);
+  if (result != -1)
+    printf("input[%d] has %d\n", result, target);
+  else
+    printf("%d is not found\n", result);
+
+  if (result == expect)
+    printf("This case is passed.\n");
+  else
+    printf("This case is failed...\n");
+}
 
 int main(int argc, char const *argv[])
 {
   int i;
   int result;
   for (i=0;i<INPUT_SIZE;i++) {
-    input[i] = i * 3;
+    input[i] = i;
+    printf("%d[%d], ", input[i], i);
   }
+  printf("\n");
   
-  result = binary_search(6);
-  printf("input[%d] has %d\n", result, 6);
+  int target;
+  
+  //Test:bin_search()
+  test_bin_search(0, 0);
+  test_bin_search(3, 3);
+  test_bin_search(9, 9);
+  test_bin_search(10, -1);
+
   return 0;
 }
+
