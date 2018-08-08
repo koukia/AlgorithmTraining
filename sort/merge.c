251e4d6f053ddf9c9dd83ac0a2913ccf9a4674e8
kohki <tk011875@gmail.com>
2018-08-09 00:20:32 +0900

    fix:reduce tmp size

--- sort/merge.c
+++ sort/merge.c
@@ -4,28 +4,36 @@
 #include "test.h"
 
 
-void merge(int* array, int begin, int end, int mid, int* work)
+void merge(int* array, int begin, int end, int mid, int* tmp)
 {
-  int i,j,k;
+  int size,i_array,i_tmp,i_hind;
 
-  for (i=begin;i<=mid;++i){
-    work[i] = array[i];
+  //配列arrayの前半部を配列tmpにコピー
+  for (i_array = begin, i_tmp = 0;i_array <= mid; i_array++, i_tmp++){
+    tmp[i_tmp] = array[i_array];
   }
+  
+  size = mid - begin + 1;
+  i_tmp = 0;
+  i_hind = mid + 1;
+  i_array = begin;
 
-  for (i=mid+1,j=end;i<=end;++i,--j){
-    work[i] = array[j];
-  }
-
-  i = begin;
-  j = end;
-  for (k=begin;k<=end;++k){
-    if (work[i] <= work[j]){
-      array[k] = work[i];
-      ++i;
+  //配列tmpと配列arrayの後半部をマージ
+  while(i_tmp < size && i_hind <= end){
+    if (tmp[i_tmp] < array[i_hind]){
+      array[i_array] = tmp[i_tmp];
+      i_tmp++;
     }else{
-      array[k] = work[j];
-      --j;
+      array[i_array] = array[i_hind];
+      i_hind++;
     }
+    i_array++;
+  }
+  //前半部(tmp)が残った場合はarrayに結合
+  while(i_tmp < size){
+    array[i_array] = tmp[i_tmp];
+    i_array++;
+    i_tmp++;
   }
 }
 
@@ -44,25 +52,32 @@ void merge_sort_rec(int *array, int begin, int end, int* work)
   merge(array, begin, end, mid, work);
 }
 
+int merge_sort(int *array,int len)
+{
+  int* work;
+  int work_size = (len-1) / 2;
+  work = malloc( sizeof(int) * work_size);
+  
+  merge_sort_rec(array, 0, len-1, work);
+  free(work);
+}
+
 int main(int argc, char const *argv[])
 {
   int len = 10;
-  int data[len];
+  int array[len];
   int i;
 
   srand(time(NULL));
 
   for (i=0;i<len;i++){
-    data[i] = rand() % 100 + 1;
-    printf("%d[%d] ", data[i], i);
+    array[i] = rand() % 100 + 1;
+    printf("%d[%d] ", array[i], i);
   }
   printf("\n");
-
-  int* work;
-  work = malloc( sizeof(int) * len );
-  merge_sort_rec(data, 0, len-1, work);
-  free(work);
-  test_sort(data, len);
+ 
+  merge_sort(array, len);
+  test_sort(array, len);
 }
 
 
